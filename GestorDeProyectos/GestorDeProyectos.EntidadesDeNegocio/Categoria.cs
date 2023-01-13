224302759c06e0d77f1184cb02abf37a3e2176e1
JennifferHerrera <jenniffer220203@gmail.com>
2023-01-13 17:50:16 -0600

    Creación y programación de las entidades de Categoria y Producto

---
+++ GestorDeProyectos/GestorDeProyectos.EntidadesDeNegocio/Categoria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestorDeProyectos.EntidadesDeNegocio
+{
+    public class Categoria
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public byte Estatus { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+
+        public List<Producto> Producto { get; set; }
+    }
+
+    public enum Estatus_Categoria
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
+    }
+}
