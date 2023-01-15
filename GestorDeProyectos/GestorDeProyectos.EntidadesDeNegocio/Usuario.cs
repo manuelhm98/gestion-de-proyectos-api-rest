569ed513fddc4fc59f618361faaf57be15577b02
GabrielaEsthefanyMoran <esthefanymoran89@gmail.com>
2023-01-15 12:43:07 -0600

    Creación y programación de las entidades de Rol y Usuario

---
+++ GestorDeProyectos/GestorDeProyectos.EntidadesDeNegocio/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestorDeProyectos.EntidadesDeNegocio
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+        [ForeignKey("Rol")]
+        [Required(ErrorMessage = "Rol es obligatorio")]
+        [Display(Name = "Rol")]
+        public int IdRol { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Apellido es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "Login es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Password es obligatorio")]
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public byte Estatus { get; set; }
+        public Rol Rol { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+    public enum Estatus_Usuario
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
+    }
+}
