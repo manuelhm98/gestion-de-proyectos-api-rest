d33dec34163bfef5afb0b45cd176c9593ad4b57b
GabrielaEsthefanyMoran <esthefanymoran89@gmail.com>
2023-01-15 12:30:48 -0600

    Creación y programación de las entidades de TipoRol y Tarea

--- GestorDeProyectos/GestorDeProyectos.EntidadesDeNegocio/Tarea.cs
+++ GestorDeProyectos/GestorDeProyectos.EntidadesDeNegocio/Tarea.cs
@@ -4,9 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GestorDeProyectos.EntidadesDeNegocio
 {
-    class Tarea
+    public class Tarea
     {
+        [Key]
+        public int IdTarea { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Descripcion es obligatorio")]
+        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+        [ForeignKey("Proyecto")]
+        [Required(ErrorMessage = "Proyecto es obligatorio")]
+        [Display(Name = "Proyecto")]
+        public int IdProyecto { get; set; }
+        [Display(Name = "Fecha De Inicio")]
+        public DateTime FechaDeInicio { get; set; }
+        [Display(Name = "Fecha De Finalizacion")]
+        public DateTime FechaDeFinalizacion { get; set; }
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public byte Estatus { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+    public enum Estatus_Tarea
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
     }
 }
--- GestorDeProyectos/GestorDeProyectos.EntidadesDeNegocio/TipoRol.cs
+++ GestorDeProyectos/GestorDeProyectos.EntidadesDeNegocio/TipoRol.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace GestorDeProyectos.EntidadesDeNegocio
 {
-    class TipoRol
+    public class TipoRol
     {
+        [Key]
+        public int IdTipoRol { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public byte Estatus { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+    public enum Estatus_TipoRol
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
     }
 }
