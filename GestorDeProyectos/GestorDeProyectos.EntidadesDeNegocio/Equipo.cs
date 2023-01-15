e88109912f461ae84d48e50f0c7dfcf48d8e91a5
GabrielaEsthefanyMoran <esthefanymoran89@gmail.com>
2023-01-15 12:57:08 -0600

    Creación y programción de las entidades de Equipo y UsuarioTarea

---
+++ GestorDeProyectos/GestorDeProyectos.EntidadesDeNegocio/Equipo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestorDeProyectos.EntidadesDeNegocio
+{
+    public class Equipo
+    {
+        [Key]
+        public int IdEquipo { get; set; }
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
+        public string Nombre { get; set; }
+        [ForeignKey("Usuario")]
+        [Required(ErrorMessage = "Usuario es obligatorio")]
+        [Display(Name = "Usuario")]
+        public int IdUsuario { get; set; }
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public byte Estatus { get; set; }
+        public Usuario Usuario { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+    public enum Estatus_Equipo
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
+    }
+}
---
+++ GestorDeProyectos/GestorDeProyectos.EntidadesDeNegocio/UsuarioTarea.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestorDeProyectos.EntidadesDeNegocio
+{
+    public class UsuarioTarea
+    {
+        [Key]
+        public int IdEquipo { get; set; }
+        [ForeignKey("Usuario")]
+        [Required(ErrorMessage = "Usuario es obligatorio")]
+        [Display(Name = "Usuario")]
+        public int IdUsuario { get; set; }
+        [ForeignKey("Tarea")]
+        [Required(ErrorMessage = "Tarea es obligatorio")]
+        [Display(Name = "Tarea")]
+        public int IdTarea { get; set; }
+        public byte Estatus { get; set; }
+        public Usuario Usuario { get; set; }
+        public Tarea Tarea { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+    public enum Estatus_UsuarioTarea
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
+    }
+}
