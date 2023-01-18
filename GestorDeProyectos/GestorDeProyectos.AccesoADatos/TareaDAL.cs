1f9e14eed80c8b681d6301a84484d721f44ceb61
GabrielaEsthefanyMoran <esthefanymoran89@gmail.com>
2023-01-17 18:36:53 -0600

    Creación y programación de TipoRolDAL y TareaDal en la capa de Acceso a Datos

---
+++ GestorDeProyectos/GestorDeProyectos.AccesoADatos/TareaDAL.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using GestorDeProyectos.EntidadesDeNegocio;
+
+namespace GestorDeProyectos.AccesoADatos
+{
+    public class TareaDAL
+    {
+        public static async Task<int> CrearAsync(Tarea pTarea)
+        {
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                bdContexto.Add(pTarea);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> ModificarAsync(Tarea pTarea)
+        {
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                var tarea = await bdContexto.Tarea.FirstOrDefaultAsync(s => s.IdTarea == pTarea.IdTarea);
+                tarea.Nombre = pTarea.Nombre;
+                tarea.Descripcion = pTarea.Descripcion;
+                tarea.IdProyecto = pTarea.IdProyecto;
+                tarea.FechaDeInicio = pTarea.FechaDeInicio;
+                tarea.FechaDeFinalizacion = pTarea.FechaDeFinalizacion;
+                bdContexto.Update(tarea);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> EliminarAsync(Tarea pTarea)
+        {
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                var tarea = await bdContexto.Tarea.FirstOrDefaultAsync(s => s.IdTarea == pTarea.IdTarea);
+                bdContexto.Tarea.Remove(tarea);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<Tarea> ObtenerPorIdAsync(Tarea pTarea)
+        {
+            var tarea = new Tarea();
+            using (var bdContexto = new BDContexto())
+            {
+                tarea = await bdContexto.Tarea.FirstOrDefaultAsync(s => s.IdTarea == pTarea.IdTarea);
+            }
+            return tarea;
+        }
+
+        public static async Task<List<Tarea>> ObtenerTodosAsync()
+        {
+            var tareas = new List<Tarea>();
+            using (var bdContexto = new BDContexto())
+            {
+                tareas = await bdContexto.Tarea.ToListAsync();
+            }
+            return tareas;
+        }
+
+        internal static IQueryable<Tarea> QuerySelect(IQueryable<Tarea> pQuery, Tarea pTarea)
+        {
+            if (pTarea.IdTarea > 0)
+                pQuery = pQuery.Where(s => s.IdTarea == pTarea.IdTarea);
+            if (!string.IsNullOrWhiteSpace(pTarea.Nombre))
+                pQuery = pQuery.Where(s => s.Nombre.Contains(pTarea.Nombre));
+            if (!string.IsNullOrWhiteSpace(pTarea.Descripcion))
+                pQuery = pQuery.Where(s => s.Descripcion.Contains(pTarea.Descripcion));
+            if (pTarea.IdProyecto > 0)
+                pQuery = pQuery.Where(s => s.IdProyecto == pTarea.IdProyecto);
+            pQuery = pQuery.OrderByDescending(s => s.IdTarea).AsQueryable();
+            if (pTarea.Estatus > 0)
+                pQuery = pQuery.Where(s => s.Estatus == pTarea.Estatus);
+            if (pTarea.Top_Aux > 0)
+                pQuery = pQuery.Take(pTarea.Top_Aux).AsQueryable();
+            return pQuery;
+        }
+
+        public static async Task<List<Tarea>> BuscarAsync(Tarea pTarea)
+        {
+            var tareas = new List<Tarea>();
+            using (var bdContexto = new BDContexto())
+            {
+                var select = bdContexto.Tarea.AsQueryable();
+                select = QuerySelect(select, pTarea);
+                tareas = await select.ToListAsync();
+            }
+            return tareas;
+
+        }
+    }
+}
---
+++ GestorDeProyectos/GestorDeProyectos.AccesoADatos/TipoRolDAL.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using GestorDeProyectos.EntidadesDeNegocio;
+
+namespace GestorDeProyectos.AccesoADatos
+{
+    public class TipoRolDAL
+    {
+        public static async Task<int> CrearAsync(TipoRol pTipoRol)
+        {
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                bdContexto.Add(pTipoRol);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> ModificarAsync(TipoRol pTipoRol)
+        {
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                var tipoRol = await bdContexto.TipoRol.FirstOrDefaultAsync(s => s.IdTipoRol == pTipoRol.IdTipoRol);
+                tipoRol.Nombre = pTipoRol.Nombre;
+                bdContexto.Update(tipoRol);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> EliminarAsync(TipoRol pTipoRol)
+        {
+            int result = 0;
+            using (var bdContexto = new BDContexto())
+            {
+                var tipoRol = await bdContexto.TipoRol.FirstOrDefaultAsync(s => s.IdTipoRol == pTipoRol.IdTipoRol);
+                bdContexto.TipoRol.Remove(tipoRol);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<TipoRol> ObtenerPorIdAsync(TipoRol pTipoRol)
+        {
+            var tipoRol = new TipoRol();
+            using (var bdContexto = new BDContexto())
+            {
+                tipoRol = await bdContexto.TipoRol.FirstOrDefaultAsync(s => s.IdTipoRol == pTipoRol.IdTipoRol);
+            }
+            return tipoRol;
+        }
+
+        public static async Task<List<TipoRol>> ObtenerTodosAsync()
+        {
+            var tipoRoles = new List<TipoRol>();
+            using (var bdContexto = new BDContexto())
+            {
+                tipoRoles = await bdContexto.TipoRol.ToListAsync();
+            }
+            return tipoRoles;
+        }
+
+        internal static IQueryable<TipoRol> QuerySelect(IQueryable<TipoRol> pQuery, TipoRol pTipoRol)
+        {
+            if (pTipoRol.IdTipoRol > 0)
+                pQuery = pQuery.Where(s => s.IdTipoRol == pTipoRol.IdTipoRol);
+            if (!string.IsNullOrWhiteSpace(pTipoRol.Nombre))
+                pQuery = pQuery.Where(s => s.Nombre.Contains(pTipoRol.Nombre));
+            pQuery = pQuery.OrderByDescending(s => s.IdTipoRol).AsQueryable();
+            if (pTipoRol.Estatus > 0)
+                pQuery = pQuery.Where(s => s.Estatus == pTipoRol.Estatus);
+            if (pTipoRol.Top_Aux > 0)
+                pQuery = pQuery.Take(pTipoRol.Top_Aux).AsQueryable();
+            return pQuery;
+        }
+
+        public static async Task<List<TipoRol>> BuscarAsync(TipoRol pTipoRol)
+        {
+            var tipoRoles = new List<TipoRol>();
+            using (var bdContexto = new BDContexto())
+            {
+                var select = bdContexto.TipoRol.AsQueryable();
+                select = QuerySelect(select, pTipoRol);
+                tipoRoles = await select.ToListAsync();
+            }
+            return tipoRoles;
+
+        }
+    }
+}
