d1d25749f61c914f953b0258a464f4fc9f1c0b5f
JennifferHerrera <jenniffer220203@gmail.com>
2023-01-18 09:22:16 -0600

    Creación y programación de PedidoBL y VentaBL en la capa Logica de Negocios

---
+++ GestorDeProyectos/GestorDeProyectos.LogicaDeNegocios/VentaBL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GestorDeProyectos.AccesoADatos;
+using GestorDeProyectos.EntidadesDeNegocio;
+
+namespace GestorDeProyectos.LogicaDeNegocios
+{
+    public class VentaBL
+    {
+        public async Task<int> CrearAsync(Venta pVenta)
+        {
+            return await VentaDAL.CrearAsync(pVenta);
+        }
+
+        public async Task<int> ModificarAsync(Venta pVenta)
+        {
+            return await VentaDAL.ModificarAsync(pVenta);
+        }
+
+        public async Task<int> EliminarAsync(Venta pVenta)
+        {
+            return await VentaDAL.EliminarAsync(pVenta);
+        }
+
+        public async Task<Venta> ObtenerPorIdAsync(Venta pVenta)
+        {
+            return await VentaDAL.ObtenerPorIdAsync(pVenta);
+        }
+
+        public async Task<List<Venta>> ObtenerTodosAsync()
+        {
+            return await VentaDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Venta>> BuscarAsync(Venta pVenta)
+        {
+            return await VentaDAL.BuscarAsync(pVenta);
+        }
+    }
+}
