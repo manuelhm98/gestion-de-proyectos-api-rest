00a1ad7fe77a5e941cfcd823e8c9d6397b78aa4a
JennifferHerrera <jenniffer220203@gmail.com>
2023-01-18 08:57:14 -0600

    Creación y programación de CategoriaBL y ProductoBL en la capa Logica de Negocios

---
+++ GestorDeProyectos/GestorDeProyectos.LogicaDeNegocios/CategoriaBL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GestorDeProyectos.AccesoADatos;
+using GestorDeProyectos.EntidadesDeNegocio;
+
+namespace GestorDeProyectos.LogicaDeNegocios
+{
+    public class CategoriaBL
+    {
+        public async Task<int> CrearAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.CrearAsync(pCategoria);
+        }
+
+        public async Task<int> ModificarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.ModificarAsync(pCategoria);
+        }
+
+        public async Task<int> EliminarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.EliminarAsync(pCategoria);
+        }
+
+        public async Task<Categoria> ObtenerPorIdAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.ObtenerPorIdAsync(pCategoria);
+        }
+
+        public async Task<List<Categoria>> ObtenerTodosAsync()
+        {
+            return await CategoriaDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Categoria>> BuscarAsync(Categoria pCategoria)
+        {
+            return await CategoriaDAL.BuscarAsync(pCategoria);
+        }
+    }
+}
---
+++ GestorDeProyectos/GestorDeProyectos.LogicaDeNegocios/ProductoBL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GestorDeProyectos.AccesoADatos;
+using GestorDeProyectos.EntidadesDeNegocio;
+
+namespace GestorDeProyectos.LogicaDeNegocios
+{
+    public class ProductoBL
+    {
+        public async Task<int> CrearAsync(Producto pProducto)
+        {
+            return await ProductoDAL.CrearAsync(pProducto);
+        }
+
+        public async Task<int> ModificarAsync(Producto pProducto)
+        {
+            return await ProductoDAL.ModificarAsync(pProducto);
+        }
+
+        public async Task<int> EliminarAsync(Producto pProducto)
+        {
+            return await ProductoDAL.EliminarAsync(pProducto);
+        }
+
+        public async Task<Producto> ObtenerPorIdAsync(Producto pProducto)
+        {
+            return await ProductoDAL.ObtenerPorIdAsync(pProducto);
+        }
+
+        public async Task<List<Producto>> ObtenerTodosAsync()
+        {
+            return await ProductoDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<Producto>> BuscarAsync(Producto pProducto)
+        {
+            return await ProductoDAL.BuscarAsync(pProducto);
+        }
+    }
+}
