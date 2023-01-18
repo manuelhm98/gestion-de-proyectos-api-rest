75259ca9ce133e89a29cbd8104e63f8dde96b0ba
GabrielaEsthefanyMoran <esthefanymoran89@gmail.com>
2023-01-18 08:38:46 -0600

    Creación y programación de EquipoBL y UsuarioTareaBL en la capa Logica de Negocio

---
+++ GestorDeProyectos/GestorDeProyectos.LogicaDeNegocios/UsuarioTareaBL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GestorDeProyectos.AccesoADatos;
+using GestorDeProyectos.EntidadesDeNegocio;
+
+namespace GestorDeProyectos.LogicaDeNegocios
+{
+    public class UsuarioTareaBL
+    {
+        public async Task<int> CrearAsync(UsuarioTarea pUsuarioTarea)
+        {
+            return await UsuarioTareaDAL.CrearAsync(pUsuarioTarea);
+        }
+
+        public async Task<int> ModificarAsync(UsuarioTarea pUsuarioTarea)
+        {
+            return await UsuarioTareaDAL.ModificarAsync(pUsuarioTarea);
+        }
+
+        public async Task<int> EliminarAsync(UsuarioTarea pUsuarioTarea)
+        {
+            return await UsuarioTareaDAL.EliminarAsync(pUsuarioTarea);
+        }
+
+        public async Task<UsuarioTarea> ObtenerPorIdAsync(UsuarioTarea pUsuarioTarea)
+        {
+            return await UsuarioTareaDAL.ObtenerPorIdAsync(pUsuarioTarea);
+        }
+        public async Task<List<UsuarioTarea>> ObtenerTodosAsync()
+        {
+            return await UsuarioTareaDAL.ObtenerTodosAsync();
+        }
+
+        public async Task<List<UsuarioTarea>> BuscarAsync(UsuarioTarea pUsuarioTarea)
+        {
+            return await UsuarioTareaDAL.BuscarAsync(pUsuarioTarea);
+        }
+    }
+}
