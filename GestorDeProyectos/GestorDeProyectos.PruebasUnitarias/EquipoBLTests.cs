70a339d8700bbfdcd0d7b1983df9a312b663e805
GabrielaEsthefanyMoran <esthefanymoran89@gmail.com>
2023-01-21 09:46:38 -0600

    Creación, programación y prueba de EquipoBLTests y UsuarioTareaBLTests en el proyecto de Pruebas Unitarias

---
+++ GestorDeProyectos/GestorDeProyectos.PruebasUnitarias/EquipoBLTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GestorDeProyectos.LogicaDeNegocios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GestorDeProyectos.EntidadesDeNegocio;
+
+namespace GestorDeProyectos.LogicaDeNegocios.Tests
+{
+    [TestClass()]
+    public class EquipoBLTests
+    {
+        private static Equipo equipoInicial = new Equipo { IdEquipo = 4, Nombre = "Ejemplo", Estatus = 1 };
+
+        [TestMethod()]
+        public async Task T1CrearAsyncTest()
+        {
+            var equipo = new Equipo();
+            equipo.Nombre = "Ejemplo";
+            equipo.IdUsuario = 1;
+            equipo.Estatus = 1;
+            int result = await new EquipoBL().CrearAsync(equipo);
+            Assert.AreNotEqual(0, result);
+        }
+
+        [TestMethod()]
+        public async Task T2ModificarAsyncTest()
+        {
+            var equipo = new Equipo();
+            equipo.IdEquipo = equipoInicial.IdEquipo;
+            equipo.Nombre = "Ejemplo Modificado";
+            equipo.IdUsuario = 1;
+            equipo.Estatus = 1;
+            int result = await new EquipoBL().ModificarAsync(equipo);
+            Assert.AreNotEqual(0, result);
+        }
+
+        [TestMethod()]
+        public async Task T3ObtenerPorIdAsyncTest()
+        {
+            var equipo = new Equipo();
+            equipo.IdEquipo = equipoInicial.IdEquipo;
+            var resultEquipo = await new EquipoBL().ObtenerPorIdAsync(equipo);
+            Assert.AreEqual(equipo.IdEquipo, resultEquipo.IdEquipo);
+        }
+
+        [TestMethod()]
+        public async Task T4ObtenerTodosAsyncTest()
+        {
+            var resultEquipo = await new EquipoBL().ObtenerTodosAsync();
+            Assert.AreNotEqual(0, resultEquipo.Count);
+        }
+
+        [TestMethod()]
+        public async Task T5BuscarAsyncTest()
+        {
+            var equipo = new Equipo();
+            equipo.Nombre = "Ejemplo";
+            equipo.IdUsuario = 1;
+            equipo.Estatus = 1;
+            var resultEquipo = await new EquipoBL().BuscarAsync(equipo);
+            Assert.AreNotEqual(0, resultEquipo.Count);
+        }
+
+        [TestMethod()]
+        public async Task T6EliminarAsyncTest()
+        {
+            var equipo = new Equipo();
+            equipo.IdEquipo = equipoInicial.IdEquipo;
+            int result = await new EquipoBL().EliminarAsync(equipo);
+            Assert.IsTrue(result != 0);
+        }
+
+    }
+}
---
+++ GestorDeProyectos/GestorDeProyectos.PruebasUnitarias/UsuarioTareaBLTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GestorDeProyectos.LogicaDeNegocios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GestorDeProyectos.EntidadesDeNegocio;
+
+namespace GestorDeProyectos.LogicaDeNegocios.Tests
+{
+    [TestClass()]
+    public class UsuarioTareaBLTests
+    {
+        private static UsuarioTarea usuarioTareaInicial = new UsuarioTarea { IdUsuarioTarea = 4, Estatus = 1 };
+
+        [TestMethod()]
+        public async Task T1CrearAsyncTest()
+        {
+            var usuarioTarea = new UsuarioTarea();
+            usuarioTarea.IdUsuario = 1;
+            usuarioTarea.IdTarea = 1;
+            usuarioTarea.Estatus = 1;
+            int result = await new UsuarioTareaBL().CrearAsync(usuarioTarea);
+            Assert.AreNotEqual(0, result);
+        }
+
+        [TestMethod()]
+        public async Task T2ModificarAsyncTest()
+        {
+            var usuarioTarea = new UsuarioTarea();
+            usuarioTarea.IdUsuarioTarea = usuarioTareaInicial.IdUsuarioTarea;
+            usuarioTarea.IdUsuario = 2;
+            usuarioTarea.IdTarea = 2;
+            usuarioTarea.Estatus = 1;
+            int result = await new UsuarioTareaBL().ModificarAsync(usuarioTarea);
+            Assert.AreNotEqual(0, result);
+        }
+
+        [TestMethod()]
+        public async Task T3ObtenerPorIdAsyncTest()
+        {
+            var usuarioTarea = new UsuarioTarea();
+            usuarioTarea.IdUsuarioTarea = usuarioTareaInicial.IdUsuarioTarea;
+            var resultUsuarioTarea = await new UsuarioTareaBL().ObtenerPorIdAsync(usuarioTarea);
+            Assert.AreEqual(usuarioTarea.IdUsuarioTarea, resultUsuarioTarea.IdUsuarioTarea);
+        }
+
+        [TestMethod()]
+        public async Task T4ObtenerTodosAsyncTest()
+        {
+            var resultUsuarioTarea = await new UsuarioTareaBL().ObtenerTodosAsync();
+            Assert.AreNotEqual(0, resultUsuarioTarea.Count);
+        }
+
+        [TestMethod()]
+        public async Task T5BuscarAsyncTest()
+        {
+            var usuarioTarea = new UsuarioTarea();
+            usuarioTarea.IdUsuario = 2;
+            usuarioTarea.IdTarea = 2;
+            usuarioTarea.Estatus = 1;
+            var resultUsuarioTarea = await new UsuarioTareaBL().BuscarAsync(usuarioTarea);
+            Assert.AreNotEqual(0, resultUsuarioTarea.Count);
+        }
+
+        [TestMethod()]
+        public async Task T6EliminarAsyncTest()
+        {
+            var usuarioTarea = new UsuarioTarea();
+            usuarioTarea.IdUsuarioTarea = usuarioTareaInicial.IdUsuarioTarea;
+            int result = await new UsuarioTareaBL().EliminarAsync(usuarioTarea);
+            Assert.IsTrue(result != 0);
+        }
+
+    }
+}
