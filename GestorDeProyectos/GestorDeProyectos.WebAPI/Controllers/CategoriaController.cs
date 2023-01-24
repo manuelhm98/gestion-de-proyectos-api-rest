203cd539d3abf9eeaf537223adec584ca5e627d7
JennifferHerrera <jenniffer220203@gmail.com>
2023-01-23 18:40:55 -0600

    Creación y Programación de los controladores de Categoria y Producto en la capa de Web API

---
+++ GestorDeProyectos/GestorDeProyectos.WebAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using GestorDeProyectos.EntidadesDeNegocio;
+using GestorDeProyectos.LogicaDeNegocios;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GestorDeProyectos.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class CategoriaController : ControllerBase
+    {
+        private CategoriaBL categoriaBL = new CategoriaBL();
+
+        [HttpGet]
+        public async Task<IEnumerable<Categoria>> Get()
+        {
+            return await categoriaBL.ObtenerTodosAsync();
+        }
+
+        [HttpGet("{idCategoria}")]
+        public async Task<Categoria> Get(int idCategoria)
+        {
+            Categoria categoria = new Categoria();
+            categoria.IdCategoria = idCategoria;
+            return await categoriaBL.ObtenerPorIdAsync(categoria);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Categoria categoria)
+        {
+            try
+            {
+                await categoriaBL.CrearAsync(categoria);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int idCategoria, [FromBody] Categoria categoria)
+        {
+            if (categoria.IdCategoria == idCategoria)
+            {
+                await categoriaBL.ModificarAsync(categoria);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{idCategoria}")]
+        public async Task<ActionResult> Delete(int idCategoria)
+        {
+            try
+            {
+                Categoria categoria = new Categoria();
+                categoria.IdCategoria = idCategoria;
+                await categoriaBL.EliminarAsync(categoria);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("Buscar")]
+        public async Task<List<Categoria>> Buscar([FromBody] object pCategoria)
+        {
+            var option = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            string strCategoria = JsonSerializer.Serialize(pCategoria);
+            Categoria categoria = JsonSerializer.Deserialize<Categoria>(strCategoria, option);
+            return await categoriaBL.BuscarAsync(categoria);
+        }
+    }
+}
